fb365421651ae7d3bc9f9f9122f8ab540157ec2a
nabeel hussain <hamdani.no02@gmail.com>
2020-07-24 13:10:32 +0500

    dashboard setting

--- src/components/Containers/MainContainer.jsx
+++ src/components/Containers/MainContainer.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 export default function MainContainer(props) {
-  return (
-    <div className="container-fluid " style={{ zoom: "92%" }}>
-      <div className="card card-accent-primary borderradius40 margintop30 custom-margin">
-        <div>
-          <div>{props.children}</div>
-        </div>
-      </div>
-    </div>
+    return (
+      <div className="container-fluid " style={{ zoom: "92%" }}>
+        <div className="card card-accent-primary borderradius40 margintop30 custom-margin">
+          <div className="panel-body">
+            <div className="row">{props.children}</div>
+          </div>
+          </div>
+          </div>
   );
 }
