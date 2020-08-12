b859e3dcb8eb6f935a63f12cef8aba25388e1710
hamid shah <hamidshah913@gmail.com>
2020-08-13 00:50:58 +0500

    finance dashboard page added

--- src/views/admin/finance/FinanceDashboard.jsx
+++ src/views/admin/finance/FinanceDashboard.jsx
@@ -28,19 +28,234 @@ export default function CodShipments(props) {
 	return response.loading ? (
 		<Loading />
 	) : (
-		transitions.map(
-			({ item, props, key }) =>
-				item && (
-					<animated.div key={key} style={props}>
-						{console.log(item)}
-						<Container>
-							<div className="card-header">
-								<h2 className="float-left">Finance Dashboard</h2>
-							</div>
-							<div className="card-body">//dashboard HTML here</div>
-						</Container>
-					</animated.div>
-				)
-		)
-	);
+			transitions.map(
+				({ item, props, key }) =>
+					item && (
+						<animated.div key={key} style={props}>
+							{console.log(item)}
+							<Container>
+								<div className="card-header">
+									<h2 className="float-left">Finance Dashboard</h2>
+								</div>
+								<div className="card-body">
+									<div className="row">
+										<div className="flex-row col-md-3">
+											<div className="thumnail-box">
+												<div className="icon color-default fs-26 mr-10 float-left">
+													<i className="fa fa-usd font40"></i>
+												</div>
+												<div className="float-left">
+													<p>
+														<span className="font20">SAR 4500/-</span>
+														<br />Sarokh Wallet</p>
+												</div>
+												<div className="clearfix"></div>
+											</div>
+										</div>
+										<div className="flex-row col-md-3">
+											<div className="thumnail-box">
+												<div className="icon color-default fs-26 mr-10 float-left">
+													<i className="fa fa-usd font40"></i>
+												</div>
+												<div className="float-left">
+													<p>
+														<span className="font20">SAR 35000/-</span>
+														<br />SAROKH PAY</p>
+												</div>
+												<div className="clearfix"></div>
+											</div>
+										</div>
+										<div className="flex-row col-md-3">
+											<div className="thumnail-box">
+												<div className="icon color-default fs-26 mr-10 float-left">
+													<i className="fa fa-usd font40"></i>
+												</div>
+												<div className="float-left">
+													<p>
+														<span className="font20">SAR 4500/-</span>
+														<br />Sarokh Wallet</p>
+												</div>
+												<div className="clearfix"></div>
+											</div>
+										</div>
+										<div className="flex-row col-md-3">
+											<div className="thumnail-box">
+												<div className="icon color-default fs-26 mr-10 float-left">
+													<i className="fa fa-usd font40"></i>
+												</div>
+												<div className="float-left">
+													<p>
+														<span className="font20">SAR 3500/-</span>
+														<br />Sarokh Wallet</p>
+												</div>
+												<div className="clearfix"></div>
+											</div>
+										</div>
+										<div className="flex-row col-md-3">
+											<div className="thumnail-box">
+												<div className="icon color-default fs-26 mr-10 float-left">
+													<i className="fa fa-usd font40"></i>
+												</div>
+												<div className="float-left">
+													<p>
+														<span className="font20">SAR 35000/-</span>
+														<br />All Drivers Wallet</p>
+												</div>
+												<div className="clearfix"></div>
+											</div>
+										</div>
+										<div className="flex-row col-md-3">
+											<div className="thumnail-box">
+												<div className="icon color-default fs-26 mr-10 float-left">
+													<i className="fa fa-usd font40"></i>
+												</div>
+												<div className="float-left">
+													<p>
+														<span className="font20">SAR 1500/-</span>
+														<br />Drivers Compensation</p>
+												</div>
+												<div className="clearfix"></div>
+											</div>
+										</div>
+										<div className="flex-row col-md-3">
+											<div className="thumnail-box">
+												<div className="icon color-default fs-26 mr-10 float-left">
+													<i className="fa fa-usd font40"></i>
+												</div>
+												<div className="float-left">
+													<p>
+														<span className="font20">SAR 25000/-</span>
+														<br />Shipper Unbilled</p>
+												</div>
+												<div className="clearfix"></div>
+											</div>
+										</div>
+										<div className="flex-row col-md-3">
+											<div className="thumnail-box">
+												<div className="icon color-default fs-26 mr-10 float-left">
+													<i className="fa fa-usd font40"></i>
+												</div>
+												<div className="float-left">
+													<p>
+														<span className="font20">SAR 1400/-</span>
+														<br />Shipper Billed</p>
+												</div>
+												<div className="clearfix"></div>
+											</div>
+										</div>
+										<div className="flex-row col-md-3">
+											<div className="thumnail-box">
+												<div className="icon color-default fs-26 mr-10 float-left">
+													<i className="fa fa-usd font40"></i>
+												</div>
+												<div className="float-left">
+													<p>
+														<span className="font20">SAR 15000/-</span>
+														<br />All Dealers Wallet</p>
+												</div>
+												<div className="clearfix"></div>
+											</div>
+										</div>
+										<div className="flex-row col-md-3">
+											<div className="thumnail-box">
+												<div className="icon color-default fs-26 mr-10 float-left">
+													<i className="fa fa-usd font40"></i>
+												</div>
+												<div className="float-left">
+													<p>
+														<span className="font20">SAR 13000/-</span>
+														<br />Dealers Compensation</p>
+												</div>
+												<div className="clearfix"></div>
+											</div>
+										</div>
+										<div className="flex-row col-md-3">
+											<div className="thumnail-box">
+												<div className="icon color-default fs-26 mr-10 float-left">
+													<i className="fa fa-usd font40"></i>
+												</div>
+												<div className="float-left">
+													<p>
+														<span className="font20">SAR 1200/-</span>
+														<br />COD Pendings</p>
+												</div>
+												<div className="clearfix"></div>
+											</div>
+										</div>
+										<div className="flex-row col-md-3">
+											<div className="thumnail-box">
+												<div className="icon color-default fs-26 mr-10 float-left">
+													<i className="fa fa-usd font40"></i>
+												</div>
+												<div className="float-left">
+													<p>
+														<span className="font20">SAR 1500/-</span>
+														<br />Vehicle Maintanace Dues</p>
+												</div>
+												<div className="clearfix"></div>
+											</div>
+										</div>
+									</div>
+									<div className="row finance-detail">
+										<div className="col-sm-12">
+											<h3 className="mb-3">View All Wallets</h3>
+											<h4>Filters</h4>
+											<div className="form-row mb-3">
+												<div className="col pl-2">
+													<input
+														type="password"
+														name="userPassword"
+														className="form-control"
+														placeholder="Select User Type (Shipper, Dealer, Driver, Vendor) (Drop down)"
+													/>
+												</div>
+												<div className="col pr-2">
+													<input
+														type="password"
+														name="userPassword"
+														className="form-control"
+														placeholder="Select User (Filter on bases of User Type) (Show all user in Drop Down)"
+													/>
+												</div>
+											</div>
+											<div className="form-row">
+												<div className="col-sm-12">
+													<div className="form-check form-check-inline">
+														<input className="form-check-input" type="checkbox" />
+														<label className="form-check-label">Driver Wallet</label>
+													</div>
+													<div className="form-check form-check-inline">
+														<input className="form-check-input" type="checkbox" />
+														<label className="form-check-label">Dealer Wallet</label>
+													</div>
+													<div className="form-check form-check-inline">
+														<input className="form-check-input" type="checkbox" />
+														<label className="form-check-label">Driver Compensation</label>
+													</div>
+													<div className="form-check form-check-inline">
+														<input className="form-check-input" type="checkbox" />
+														<label className="form-check-label">Dealer Compensation</label>
+													</div>
+													<div className="form-check form-check-inline">
+														<input className="form-check-input" type="checkbox" />
+														<label className="form-check-label">Shipper COD</label>
+													</div>
+													<div className="form-check form-check-inline">
+														<input className="form-check-input" type="checkbox" />
+														<label className="form-check-label">Shipper Delivery Charges</label>
+													</div>
+													<div className="form-check form-check-inline">
+														<input className="form-check-input" type="checkbox" />
+														<label className="form-check-label">Vendor Pay</label>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</Container>
+						</animated.div>
+					)
+			)
+		);
 }
