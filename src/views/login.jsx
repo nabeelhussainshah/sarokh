04b98ac00abd61f89d80d55ad1c548784a79a27e
hamid shah <hamidshah913@gmail.com>
2020-09-13 14:26:24 +0500

    login page changed

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -65,65 +65,69 @@ function Login(props) {
 		<Fragment>
 			<div className="limiter">
 				<div className="container-login100">
-					<div className="login100-more">
-						<form onSubmit={onSubmit} autoComplete="on">
-							<fieldset>
-								<h1>Sign In to your account</h1>
-								<div className="mb-3">
-									<div className="form-group">
-										<label>User Name</label>
-										<input
-											name="username"
-											type="text"
-											className="form-control"
-											formcontrolname="username"
-											placeholder="Username"
-											autoComplete="username"
-											required
-										/>
+					<div className="col-md-8 p-0">
+						<div className="wrap-login100">
+							<img src={require('../assets/images/logo.png')} />
+						</div>
+					</div>
+					<div className="col-md-4 p-0 brownbg">
+						<div className="login100-more">
+							<form onSubmit={onSubmit} autoComplete="on">
+								<fieldset>
+									<h1>Sign In to your account</h1>
+									<div className="mb-3">
+										<div className="form-group">
+											<label>User Name</label>
+											<input
+												name="username"
+												type="text"
+												className="form-control"
+												formcontrolname="username"
+												placeholder="Username"
+												autoComplete="username"
+												required
+											/>
+										</div>
 									</div>
-								</div>
-								<div className="mb-4">
-									<div className="form-group">
-										<label>Password</label>
-										<input
-											name="password"
-											type="password"
-											className="form-control"
-											formcontrolname="password"
-											placeholder="Password"
-											autoComplete="current-password"
-											required
-										/>
+									<div className="mb-4">
+										<div className="form-group">
+											<label>Password</label>
+											<input
+												name="password"
+												type="password"
+												className="form-control"
+												formcontrolname="password"
+												placeholder="Password"
+												autoComplete="current-password"
+												required
+											/>
+										</div>
 									</div>
-								</div>
-								<div className="col-12">
-									<input
-										style={{ marginRight: '5px' }}
-										type="checkbox"
-										className=" px-0"
-									/>
-									<label>Remember me</label>
-								</div>
-								<button
-									type="submit"
-									className="btn btn-danger px-4 float-right"
-								>
-									Login
+									<div className="col-12">
+										<input
+											style={{ marginRight: '5px' }}
+											type="checkbox"
+											className=" px-0"
+										/>
+										<label>Remember me</label>
+									</div>
+									<button
+										type="submit"
+										className="btn btn-danger px-4 float-right"
+									>
+										Login
 								</button>
-								<div className="clearfix"></div>
-								<div className="col-12">
-									<button type="button" className="btn btn-link px-0">
-										Forgot password?
+									<div className="clearfix"></div>
+									<div className="col-12">
+										<button type="button" className="btn btn-link px-0">
+											Forgot password?
 									</button>
-								</div>
-							</fieldset>
-						</form>
+									</div>
+								</fieldset>
+							</form>
+						</div>
 					</div>
 
-					<div className="wrap-login100">
-						<img src={require('../assets/images/logo.png')} />
-					</div>
 				</div>
 			</div>
 		</Fragment>
