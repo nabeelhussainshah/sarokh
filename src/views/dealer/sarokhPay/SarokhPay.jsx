4d8db9da613669cddc14948f3e9ba27dc35efcb0
nabeelhussainshah <hamdani.no02@gmail.com>
2020-12-24 10:50:54 +0500

    dealer code merged

--- src/views/dealer/sarokhPay/SarokhPay.jsx
+++ src/views/dealer/sarokhPay/SarokhPay.jsx
@@ -7,12 +7,18 @@ import { toast } from 'react-toastify';
 import moment from 'moment';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import { getBillApi, recordBillPaymentApi } from '../../../Api/dealerApi';
+import {
+	getBillApi,
+	recordBillPaymentApi,
+	getBillDetailsApi,
+} from '../../../Api/dealerApi';
 
 export default function SarokhPay(props) {
 	const hist = useHistory();
 	const [response, setresponse] = useState({ loading: false });
 	const [billId, setBillId] = useState('');
+	const [billStatus, setBillStatus] = useState('');
+	const [billTotalAmount, setbillTotalAmount] = useState(0);
 
 	const sarokhPay = JSON.parse(localStorage.getItem('sarokhPay'));
 
@@ -42,8 +48,14 @@ export default function SarokhPay(props) {
 
 	const onSubmitForm = async (values) => {
 		setBillId(values.billNo);
-		await getBillApi(values).then((res) => {
-			console.log('Res => ', res);
+		await getBillDetailsApi(values).then((res) => {
+			var totalAmount = 0;
+			res.billSummary.map((data, index) => {
+				totalAmount = totalAmount + data.amount;
+			});
+			setBillStatus(res.status);
+
+			setbillTotalAmount(totalAmount);
 		});
 	};
 
@@ -72,7 +84,11 @@ export default function SarokhPay(props) {
 					<animated.div key={key} style={props}>
 						<Container>
 							<div className="card-header">
-								<h2 className="float-left">Sarokh Pay</h2>
+								{localStorage.getItem('Language') != 'Arabic' ? (
+									<h2 className="float-left">Sarokh Pay</h2>
+								) : (
+									<h2 className="float-left">صاروخ باي</h2>
+								)}
 							</div>
 							<div className="card-body">
 								<Formik
@@ -88,7 +104,11 @@ export default function SarokhPay(props) {
 											<Form>
 												<div className="row">
 													<div className="col-md-6">
-														<label for="trackingNo">Bill No</label>
+														{localStorage.getItem('Language') != 'Arabic' ? (
+															<label for="trackingNo">Bill No</label>
+														) : (
+															<label for="trackingNo">مشروع قانون لا</label>
+														)}
 														<Field
 															name="billNo"
 															type="text"
@@ -107,7 +127,12 @@ export default function SarokhPay(props) {
 														/>
 													</div>
 													<div className="col-md-6">
-														<label htmlFor="email">Biller</label>
+														{localStorage.getItem('Language') != 'Arabic' ? (
+															<label htmlFor="email">Biller</label>
+														) : (
+															<label htmlFor="email">بيلر</label>
+														)}
+
 														<Field
 															name="biller"
 															as="select"
@@ -127,9 +152,15 @@ export default function SarokhPay(props) {
 														/>
 													</div>
 												</div>
-												<button className="btn btn-info float-right btnbrown mt-2">
-													Search
-												</button>
+												{localStorage.getItem('Language') != 'Arabic' ? (
+													<button className="btn btn-info float-right btnbrown mt-2">
+														Search
+													</button>
+												) : (
+													<button className="btn btn-info float-right btnbrown mt-2">
+														بحث
+													</button>
+												)}
 											</Form>
 										);
 									}}
@@ -137,50 +168,50 @@ export default function SarokhPay(props) {
 
 								<div className="clearfix"></div>
 								<div className="form-row">
-									<div className="col-sm-6">
-										<div className="row">
-											<label className="col-sm-6 col-6 redcolor">
-												Bill No:
-											</label>
-											<label className="col-sm-6 col-6">
-												<p className=" text-left">
-													{sarokhPay && sarokhPay.trackingNo
-														? sarokhPay.trackingNo
-														: ''}
-												</p>
-											</label>
-										</div>
-									</div>
-									<div className="col-sm-6">
-										<label className="col-sm-6 col-6 redcolor">
-											Biller Name:
-										</label>
-										<label className="col-sm-6 col-6">
-											<p className=" text-left">
-												{sarokhPay && sarokhPay.senderName
-													? sarokhPay.senderName
-													: ''}
-											</p>
-										</label>
-									</div>
-									<div className="col-sm-12 right-align">
-										<label className="col-sm-6 col-6 redcolor">Amount:</label>
-										<label className="col-sm-6 col-6 text-right">
-											Sar{' '}
-											{sarokhPay && sarokhPay.totalAmount
-												? sarokhPay.totalAmount
-												: ''}
-											/-
-										</label>
-									</div>
-									<div className="col-md-12">
-										<button
-											className="btn btn-info float-right btnbrown mt-2"
-											onClick={recordBillPaymentHandler}
-										>
-											Paid
-										</button>
-									</div>
+									{billStatus === 'UnPaid' ? (
+										<>
+											<div className="col-sm-6">
+												<div className="row">
+													<label className="col-sm-6 col-6 redcolor">
+														Bill No:
+													</label>
+													<label className="col-sm-6 col-6">
+														<p className=" text-left">{billId ? billId : ''}</p>
+													</label>
+												</div>
+											</div>
+											<div className="col-sm-6">
+												<label className="col-sm-6 col-6 redcolor">
+													Biller Name:
+												</label>
+												<label className="col-sm-6 col-6">
+													<p className=" text-left">
+														{sarokhPay && sarokhPay.senderName
+															? sarokhPay.senderName
+															: 'Sarokh'}
+													</p>
+												</label>
+											</div>
+											<div className="col-sm-12 right-align">
+												<label className="col-sm-6 col-6 redcolor">
+													Amount:
+												</label>
+												<label className="col-sm-6 col-6 text-right">
+													Sar {billTotalAmount} /-
+												</label>
+											</div>
+											<div className="col-md-12">
+												<button
+													className="btn btn-info float-right btnbrown mt-2"
+													onClick={recordBillPaymentHandler}
+												>
+													Paid
+												</button>
+											</div>
+										</>
+									) : (
+										<>{billStatus === '' ? '' : <h1>Already paid</h1>}</>
+									)}
 								</div>
 							</div>
 						</Container>
